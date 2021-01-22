bb5bb2d26ce5fbd0f25a1e76b435f8f8628a4064
IvanSklepic <ivan.sklepic96@gmail.com>
2021-01-22 16:23:04 +0100

    LV09

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,11 +1,35 @@
 @extends('layouts.app')
 
+@php
+$loggedInUser = \Auth::user();
+@endphp
+
 @section('content')
+
 <div class="mt-5">
     <h1 class="text-center">{{ $user->name }}</h1>
     <img src="https://picsum.photos/300" alt="profile image" class="d-block mx-auto rounded-circle">
     
     <div class="btn-group mt-5" role="group">
         <a class="btn btn-secondary" href="{{ route('users.index') }}">Back</a>
-  </div>  
+
+        
+        @if($loggedInUser->isAdmin() || $user->id === $loggedInUser->id)
+        <a class="btn btn-primary" href="{{ route('users.edit', ['user' => $user]) }}">Edit</a>
+        <a class="btn btn-primary" href="{{ route('change_password.edit', ['user' => $user]) }}">Change Password</a>
+        @endif
+
+        
+        @if($loggedInUser->isAdmin() || $user->id === $loggedInUser->id)
+        <form class="form-inline" action="{{ route('users.destroy', ['user' => $user->id]) }}" method="POST">
+          
+            @csrf
+            @method('DELETE')
+            <button type="submit" onclick="confirm('Are you sure?')" class="btn btn-danger">Deactivate</button>
+        </form>
+        @endif
+    </div>
+
+    
+</div>
 @endsection
